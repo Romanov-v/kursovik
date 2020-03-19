28c0b8130d8355bbfda496c44706dc151c6c0a6e
Pavel <pasha-super2001@yandex.ru>
2020-03-19 21:01:21 +0300

    добавил алгоритм Гост

--- main.c
+++ main.c
@@ -8,16 +8,65 @@
 
 int main (int argc, char** argv)
 {
-	assert(argc > 1);
-	assert( (access(argv[1], F_OK)) != -1 && "файла не существует") ;
+    assert(argc > 1);
+    assert( (access(argv[1], F_OK)) != -1 && "файла не существует") ;
 
-	// затирание
-	//readFileBytes(argv[1]);
-	gost(argv[1]);
-	//writebyte(f, 0x55);
-	//writebyte(f, 0xAA);
+    FILE * f;
+
+    f = fopen(argv[1], "w");
+    fprintf(f, "aaaaaa");
+    fclose(f);
+    // затирание
+    gost(argv[1]);
+    //writebyte(f, 0x55);
+    //writebyte(f, 0xAA);
+
+    int user(int argc, char** argv)
+    {
+
+        printf("Do you want to erase information with your data? \n 1 - yes 2 - no \n");
+
+        int h;
+        scanf("%d", &h);
+        if (h == 1) {
+            printf("Введте свои значения");
+            int o = 0;
+            int j = 0;
+            int f = 0;
+            scanf("%d %d %d", &o, &j, &f);
+            int user(char *filename);
+            {
+                char newbytes[] = {o, j, f};
+                algorithm(argv[1], newbytes, 3);
+                return 0;
+            }
+
+        } else if (h > 1)
+        {
+            printf(" Choose one of the ways \n 1 - GOST 2 - VSITR 3 - NAVSO 4 - SHNAIDER \n");
+            int g;
+            scanf("%d", &g);
+            if (g > 4) {
+                int shnaider(char *filename);
+            } else if (g < 1) {
+                int gost(char *filename);
+
+            } else if (g == 2) {
+                int vsitr(char *filename);
+
+            } else {
+                int navso(char *filename);
+            }
+            return 0;
 
 
-	return 0;
+        }
+    }
+
+
+    return 0;
 }
+
+
+
 
